8ed04c8f608b7a2b3a0e86be2d90cb21c0b90ceb
Ogbuanya Austine <diribe3000@gmail.com>
2017-11-09 17:02:57 +0100

    Added route

---
+++ src/routes/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const Home = ({ data: { loading, allUsers } }) =>
+    ( loading ? null : allUsers.map(u => <h1 key={u.id}>{u.email}</h1>));
+
+const allUsersQuery = gql`{ 
+    allUsers 
+        { 
+            id
+            email
+        } 
+    }
+`;
+
+export default graphql(allUsersQuery)(Home);
