4e34efeb40288f8c3fa078406c5a14f8d54be537
Ogbuanya Austine <diribe3000@gmail.com>
2017-11-12 07:21:11 +0100

    Added onSubmit and onChange methods

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -13,12 +13,23 @@ export default observer(class Login extends React.Component {
         });
     }
 
+    onsubmit = () => {
+        const { email, password } = this;
+        console.log(email);
+        console.log(password);
+    };
+
+    onChange = e => {
+        const { name, value } = e.target;
+        this[name] = value;
+    }
+
     render() {
 
         const { email, password } = this;
         return (
             <Container text>
-                <Header as="h2">Register</Header>
+                <Header as="h2">Login</Header>
                 <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
                 <Input name="password" onChange={this.onChange} value={password} type="password" placeholder="Password" fluid />
                 <Button onClick={this.onSubmit} >Submit</Button>
